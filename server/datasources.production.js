a77553efb63c4819561dbba3560ed2a4897faaf6
Tibet Sprague <tibet.sprague@gmail.com>
2018-03-07 15:33:10 -0800

    Turn off explorer on production
    Little bits of cleanup

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,10 +2,5 @@ module.exports = {
   postgresql: {
     connector: 'postgresql',
     url: process.env.DATABASE_URL
-    //host: process.env.DB_HOST || 'localhost',
-    //port: 5432,
-    //user: process.env.DB_USER,
-    //password: process.env.DB_PASSWORD,
-    //database: 'alchemy',
   }
 };
