d5995042ee7547440c94ed5e946cbdde5a984cf6
Tibet Sprague <tibet.sprague@gmail.com>
2018-11-08 12:24:15 -0800

    Fix error when setting up database

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -3,6 +3,6 @@ var ds = server.dataSources.postgresql;
 var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
-  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
   ds.disconnect();
 });
