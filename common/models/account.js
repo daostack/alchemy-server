257b9f5dadeaad49eebe64c47985f71ca077c055
Tibet Sprague <tibet.sprague@gmail.com>
2019-01-29 10:37:41 -0800

    Try to fix bug when running alchemy-server in docker

--- common/models/account.js
+++ common/models/account.js
@@ -7,19 +7,21 @@ module.exports = function(Account) {
     if (ctx.options.skipSignatureCheck) {
       next();
     } else {
+      const data = ctx.instance ? ctx.instance : ctx.data;
+
       // Check that timestamp is within the last 10 minutes
-      const timestamp = parseInt(ctx.data.timestamp);
+      const timestamp = parseInt(data.timestamp);
       const now = new Date().getTime();
       if (now - timestamp > 10 * 60 * 1000) {
         next(new Error('Invalid signature'));
         return;
       }
 
-      const text = "Please sign this message to confirm your request to update your profile to name '" + ctx.data.name + "' and description '" + ctx.data.description + "'. There's no gas cost to you. Timestamp:" + ctx.data.timestamp;
+      const text = "Please sign this message to confirm your request to update your profile to name '" + data.name + "' and description '" + data.description + "'. There's no gas cost to you. Timestamp:" + data.timestamp;
       const msg = ethUtil.bufferToHex(Buffer.from(text, 'utf8'));
-      const recoveredAddress = sigUtil.recoverPersonalSignature({ data: msg, sig: ctx.data.signature });
+      const recoveredAddress = sigUtil.recoverPersonalSignature({ data: msg, sig: data.signature });
 
-      if (recoveredAddress == ctx.data.ethereumAccountAddress) {
+      if (recoveredAddress == data.ethereumAccountAddress) {
         next();
       } else {
         next(new Error('Must include valid signature to update your account profile.'))
